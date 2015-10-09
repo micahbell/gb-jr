fd1b13063a09a8c6c2bf81a4c5b1773c2e9582a3
Micah Bell <micahmbell@gmail.com>
2015-10-09 09:37:45 -0600

    Add sign up form

--- public/javascripts/controllers/accountController.js
+++ public/javascripts/controllers/accountController.js
@@ -1,7 +1,11 @@
-app.controller('AccountController', function($scope, $state) {
+app.controller('AccountController', function($scope, $state, $http) {
   $scope.color='Blue';
-
   $scope.glazes = function() {
     $state.go('layout');
+  }
+
+  $scope.showForm = false;
+  $scope.toggleSignUp = function() {
+    $scope.showForm = !$scope.showForm;
   }
 });
